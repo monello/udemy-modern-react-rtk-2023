03f786a328ea43cf38d5c651759a104a6b42fc64
Morne Louw <louw.morne@gmail.com>
2023-03-28 08:02:18 +0200

    chore: clean up the code

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
-// 1) Import the React and ReactDOM libraries
-// - The React library defines what a component is and how multiple components work together
 import React from "react";
-// - ReactDOM library that knows how to get a component to show up in a browser
 import ReactDOM from "react-dom/client";
 
-// 2) Get a reference to the div with the id "root"
 const rootEl = document.getElementById('root');
-
-// 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(rootEl);
 
-// 4) Create as component
 function App() {
-    let message = 'Bye there!';
-    if (Math.random() > 0.5) {
-        message = 'Hi there!';
-    }
-    const someNumber = 1234;
-    const someBoolean = true;
-    const someArray = [1, 2, 3];
-    const someObject = {
-        foo: "foo",
-        bar: "bar"
-    };
-    return <>
-        <h1>{message}</h1>
-        <p>Some number: {someNumber}</p>
-        <p>Some boolean: {someBoolean}</p>
-        <p>Some array: {someArray}</p>
-        {/* <p>Some object: {someObject}</p> */}
-        <p>Time: {new Date().toLocaleTimeString()}</p>
-    </>;
+    return <h1>Hello World!</h1>;
 }
 
-// 5) Show the component on the screen
 root.render(<App />);
