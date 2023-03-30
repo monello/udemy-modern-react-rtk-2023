bbbca23b65e575ee3509c64784db67d4f6084ef2
Morne Louw <louw.morne@gmail.com>
2023-03-30 07:11:06 +0200

    extract component for better code organization

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const rootEl = document.getElementById('root');
 const root = ReactDOM.createRoot(rootEl);
-
-function App() {
-    return <>
-        <p>This should render an input that takes only numbers from 5 to 10 as input</p>
-        <input
-            type="number"
-            min={5}
-            max={10}
-            style={{ border: "1px solid red" }}
-        />
-    </>;
-}
 
 root.render(<App />);
