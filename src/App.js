d64f852eeff0dc83a34fdededcfa1a47acde0db0
Morne Louw <louw.morne@gmail.com>
2023-03-30 21:37:56 +0200

    create intial core components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ProfileCard from "./ProfileCard";
+
+function App() {
+    return <div>
+        <div>Personal Digital Assistants</div>
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+    </div>;
+}
+
+export default App;
