993f6a612073c41f14c67b1ac5d465cdb0af870b
Morne Louw <louw.morne@gmail.com>
2023-03-30 06:56:22 +0200

    customizing components with props

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ const rootEl = document.getElementById('root');
 const root = ReactDOM.createRoot(rootEl);
 
 function App() {
-    return <h1>Hello World!</h1>;
+    return <>
+        <p>This should render an input that takes only numbers from 5 to 10 as input</p>
+        <input
+            type="number"
+            min={5}
+            max={10}
+            style={{ border: "1px solid red" }}
+        />
+    </>;
 }
 
 root.render(<App />);
