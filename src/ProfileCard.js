27bd5ce15a8d7f8fa73b63875bde7ae92b737ba4
Morne Louw <louw.morne@gmail.com>
2023-03-30 22:25:35 +0200

    removed redundant template string in image alt attr

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,6 @@
 function ProfileCard({ title, handle, image }) {
     return <div>
-        <div><img src={image} alt={`${title}`} /></div>
+        <div><img src={image} alt={title} /></div>
         <div>The title is: {title}</div>
         <div>The handle is: {handle}</div>
     </div>;
