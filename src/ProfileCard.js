04f1202337f30f21bcb52402cc9e4320f9ef8689
Morne Louw <louw.morne@gmail.com>
2023-03-30 21:53:05 +0200

    adding props to our profile-card component

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,8 @@
-function ProfileCard() {
-    return <div>Profile Card</div>;
+function ProfileCard(props) {
+    return <div>
+        <div>The title is: {props.title}</div>
+        <div>The handle is: {props.handle}</div>
+    </div>;
 }
 
 export default ProfileCard;
