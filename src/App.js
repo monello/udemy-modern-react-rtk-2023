ccccc49f2408ee9004b97f1d027fc0c8f8e96672
Morne Louw <louw.morne@gmail.com>
2023-04-01 08:40:35 +0200

    implement state to update a value on screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 function App() {
+    const [count, setCount] = useState(0);
     const handleClick = () => {
         console.log("Button Clickied");
+        setCount(count + 1);
     };
     return <h1>
         <button onClick={handleClick}>Add Animal</button>
+        <p>Animals to show: {count}</p>
     </h1>;
 }
 
