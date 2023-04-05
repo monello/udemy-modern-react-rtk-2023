a27040f457d4d3d0ac0ead877e2e7789d4592177
Morne Louw <louw.morne@gmail.com>
2023-04-05 14:18:24 +0200

    feat: show svg for each animal

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,18 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import fish from './svg/fish.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import heart from './svg/heart.svg';
+
+const svgMap = { bird, cat, cow, dog, fish, gator, horse };
+
 function AnimalShow({ type }) {
-    return <div>{type}</div>;
+    return <div>
+        <img alt="animal" src={svgMap[type]} />
+    </div>;
 }
 
 export default AnimalShow;
