e89268a72d29ea81323312ba39550facef894c7b
Morne Louw <louw.morne@gmail.com>
2023-04-05 13:42:48 +0200

    feat: generate random animal function and update state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse', 'fish'];
+    const animal = Math.floor(Math.random() * animals.length);
+    return animals[animal];
+}
+
 function App() {
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
+
     const handleClick = () => {
-        setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()]);
     };
     return <h1>
         <button onClick={handleClick}>Add Animal</button>
-        <p>Animals to show: {count}</p>
+        <div>{animals}</div>
     </h1>;
 }
 
