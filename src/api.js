b32daafd596f03e706f7b055cdab1ee4220ef7cf
Morne Louw <louw.morne@gmail.com>
2023-04-07 14:23:07 +0200

    clean out test code, make search term a parameter

--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID tUyaUjPJaxPAPs94RMmVpTtsrouATF3q5VTfmo6FJD4'
         },
         params: {
-            query: 'fish'
+            query: term
         }
     });
-    console.log(response);
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
