435a4e744762e85df5c246ec572010022d6a4c2b
Morne Louw <louw.morne@gmail.com>
2023-04-08 22:34:46 +0200

    state manageemnt to add a new book

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 export const App = () => {
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        const updatedBooks = [
+            ...books,
+            title
+        ];
+        setBooks(updatedBooks);
+    };
+
+    const booksList = books.map((book, index) => <li key={index}>{book}</li>);
+
     return (
-        <div>Books App</div>
+        <>
+            <div>Books App</div>
+            {!!books.length && <><h1>Books</h1><ul>{booksList}</ul></>}
+            <BookCreate onCreate={createBook} />
+        </>
     );
 };
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const BookCreate = () => {
+const BookCreate = ({ onCreate }) => {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    };
     return (
-        <div>BookCreate</div>
+        <div className='book-create'>
+            <h3>Add a Book</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input className='input' value={title} onChange={handleChange} />
+                <button className='button' type='submit'>Submit</button>
+            </form>
+        </div>
     );
 };
 
