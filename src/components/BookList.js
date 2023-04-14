f8d4cac8ed16de49552b5e7164d79e267e51eaea
Morne Louw <louw.morne@gmail.com>
2023-04-14 15:01:49 +0200

    react context - create, provide and consume a simple example

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 const BookList = ({ books, onDelete, onEdit }) => {
+    const value = useContext(BooksContext);
+
     const renderedBooks = books.map(book => <BookShow
         key={book.id}
         book={book}
@@ -9,7 +13,10 @@ const BookList = ({ books, onDelete, onEdit }) => {
         onEdit={onEdit}
     />);
     return (
-        <div className='book-list'>{renderedBooks}</div>
+        <div className='book-list'>
+            {value}
+            {renderedBooks}
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import BooksContext from './context/books';
+
 import './index.css';
+
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+    <BooksContext.Provider value={5}>
+        <App />
+    </BooksContext.Provider>
+);
