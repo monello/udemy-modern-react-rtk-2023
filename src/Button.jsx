1fc5953ab454f2535d4046e0cf02b8a72dd32eba
Morne Louw <louw.morne@gmail.com>
2023-04-18 17:17:08 +0200

    define the props we need for the different button types and variations

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-const Button = ({ children }) => {
+const Button = ({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+}) => {
     return <button>{children}</button>;
 };
 
