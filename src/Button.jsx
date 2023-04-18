677e13ecb782553894ce77d061e3cce62cf32a68
Morne Louw <louw.morne@gmail.com>
2023-04-18 18:54:46 +0200

    style a primary button using tailwind css classes

--- src/Button.jsx
+++ src/Button.jsx
@@ -11,7 +11,11 @@ const Button = ({
     outline,
     rounded,
 }) => {
-    return <button className="bg-red-500">{children}</button>;
+    return (
+        <button className="px-3 py-1.5 border border-blue-500 bg-blue-500 text-white">
+            {children}
+        </button>
+    );
 };
 
 Button.propTypes = {
