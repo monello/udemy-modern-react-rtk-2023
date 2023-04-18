048df2eb4797833bf31a2c24fd6f55c255326ade
Morne Louw <louw.morne@gmail.com>
2023-04-18 19:47:29 +0200

    applying the rest of the style using the classnames and tailwind packages

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,127 @@ import Button from "./Button";
 const App = () => {
     return (
         <div>
+            <h1 className="text-lg font-bold leading-10 underline">
+                Variations
+            </h1>
             <div>
-                <Button primary>Button Text</Button>
+                <Button>Default</Button>
             </div>
             <div>
-                <Button secondary>Button Text</Button>
+                <Button primary>Primary</Button>
             </div>
             <div>
-                <Button success>Button Text</Button>
+                <Button secondary>Secondary</Button>
             </div>
             <div>
-                <Button warning>Button Text</Button>
+                <Button success>Success</Button>
             </div>
             <div>
-                <Button danger>Button Text</Button>
+                <Button warning>Warning</Button>
             </div>
-            <div></div>
+            <div>
+                <Button danger>Danger</Button>
+            </div>
+            <br />
+            <h1 className="text-lg font-bold leading-10 underline">
+                Rounded Style
+            </h1>
+            <div>
+                <Button rounded>Default Rounded</Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    Primary Rounded
+                </Button>
+            </div>
+            <div>
+                <Button secondary rounded>
+                    Secondary Rounded
+                </Button>
+            </div>
+            <div>
+                <Button success rounded>
+                    Success Rounded
+                </Button>
+            </div>
+            <div>
+                <Button warning rounded>
+                    Warning Rounded
+                </Button>
+            </div>
+            <div>
+                <Button danger rounded>
+                    Danger Rounded
+                </Button>
+            </div>
+            <br />
+            <h1 className="text-lg font-bold leading-10 underline">
+                Outline Style
+            </h1>
+            <div>
+                <Button primary outline>
+                    Default Outline
+                </Button>
+            </div>
+            <div>
+                <Button primary outline>
+                    Primary Outline
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    Secondary Outline
+                </Button>
+            </div>
+            <div>
+                <Button success outline>
+                    Success Outline
+                </Button>
+            </div>
+            <div>
+                <Button warning outline>
+                    Warning Outline
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    Danger Outline
+                </Button>
+            </div>
+            <br />
+            <h1 className="text-lg font-bold leading-10 underline">
+                Rounded Outline Style
+            </h1>
+            <div>
+                <Button primary rounded outline>
+                    Default Rounded Outline
+                </Button>
+            </div>
+            <div>
+                <Button primary rounded outline>
+                    Primary Rounded Outline
+                </Button>
+            </div>
+            <div>
+                <Button secondary rounded outline>
+                    Secondary Rounded Outline
+                </Button>
+            </div>
+            <div>
+                <Button success rounded outline>
+                    Success Rounded Outline
+                </Button>
+            </div>
+            <div>
+                <Button warning rounded outline>
+                    Warning Rounded Outline
+                </Button>
+            </div>
+            <div>
+                <Button danger rounded outline>
+                    Danger Rounded Outline
+                </Button>
+            </div>
         </div>
     );
 };
--- src/Button.jsx
+++ src/Button.jsx
@@ -17,6 +17,14 @@ const Button = ({
         "border-green-500 bg-green-500 text-white": success,
         "border-yellow-400 bg-yellow-400 text-white": warning,
         "border-red-500 bg-red-500 text-white": danger,
+        "rounded-full": rounded,
+        // Note: classes listed later will overwrite classes listed earlier, in this case bg (background) will be used over other bd-xxx
+        "bg-white": outline,
+        "text-blue-500": outline && primary,
+        "text-gray-900": outline && secondary,
+        "text-green-500": outline && success,
+        "text-yellow-400": outline && warning,
+        "text-red-500": outline && danger,
     });
     return <button className={classes}>{children}</button>;
 };
