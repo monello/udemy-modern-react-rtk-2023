98488712189e0f82431aaa3c647518e68a5127d4
Morne Louw <louw.morne@gmail.com>
2023-04-20 21:58:17 +0200

    implement the select-option functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 const App = () => {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
     const options = [
         { label: "Red", value: "red" },
         { label: "Green", value: "green" },
         { label: "Blue", value: "blue" },
     ];
-    return <Dropdown options={options} />;
+
+    return (
+        <Dropdown
+            options={options}
+            selection={selection}
+            onSelect={handleSelect}
+        />
+    );
 };
 
 export default App;
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selection, onSelect }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
         setIsOpen((prevState) => !prevState);
     };
 
+    const handleSelect = (selectedOption) => {
+        setIsOpen(false);
+        onSelect(selectedOption);
+    };
+
     const optionElements = options.map((option) => (
-        <div key={option.value}>{option.label}</div>
+        <div key={option.value} onClick={() => handleSelect(option)}>
+            {option.label}
+        </div>
     ));
+
+    const content = selection ? selection.label : "Select ...";
 
     return (
         <div>
-            <div onClick={handleClick}>Select Option</div>
+            <div onClick={handleClick}>{content}</div>
             {isOpen && <div>{optionElements}</div>}
         </div>
     );
