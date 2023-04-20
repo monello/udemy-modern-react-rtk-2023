c81bcabecb8dc854b391a8dce60783ba41786202
Morne Louw <louw.morne@gmail.com>
2023-04-20 06:15:26 +0200

    mapped list - onClick variation 2

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -6,9 +6,13 @@ const Accordion = ({ items }) => {
     const tabs = items.map((item, index) => {
         const isExpanded = index === expandedTab;
 
+        const handleClick = () => {
+            setExpendedTab(index);
+        };
+
         return (
             <div key={item.id}>
-                <div onClick={() => setExpendedTab(index)}>
+                <div onClick={handleClick}>
                     {item.label} {isExpanded ? "EXPANDED!" : "COLLAPSED"}
                 </div>
                 {isExpanded && <div>{item.content}</div>}
