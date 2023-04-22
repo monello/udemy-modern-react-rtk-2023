fb74aa79031c5dc1f39a39833bdb5de311f24860
Morne Louw <louw.morne@gmail.com>
2023-04-22 09:46:34 +0200

    create navigate function and share NavigationContext with the rest of the app

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -3,8 +3,13 @@ import { createContext, useEffect, useState } from "react";
 const NavigationContext = createContext();
 
 export const NavigationProvider = ({ children }) => {
+    // currentPath will be used (soon) to make react navigate to whatever the value in currentPath is
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
+    // This useEffect covers scenarios where the user clicks the browser's Back and Forward buttons
+    // When there buttons are clicked (User Navigation) the `popstate` event is fired
+    // We can then get the path that the user "navigated" to from the state which is returned from (popped) from the browser's History State
+    // We use this then to keep our currentPath state variable syncedS
     useEffect(() => {
         const handler = () => {
             setCurrentPath(window.location.pathname);
@@ -17,8 +22,28 @@ export const NavigationProvider = ({ children }) => {
         };
     }, []);
 
+    // The purpuse of the navigate function will be to handle all scenarios where we need to navigate programatically.
+    // This could be user-driven by clicks (or ay actions) inside our app. This is calls Programatic navigation
+    // We push the target path (destination we need to navigate to - hence the prop is called "to") to the browser's History State
+    // pushState does not case the popstate event to fire, so we need to manually sync our currentPath state variable
+    const navigate = (to) => {
+        window.history.pushState({}, "", to);
+        // setting the currentPath that will soon be shared to the rest of the app so that React can use it to render the correct components
+        setCurrentPath(to);
+    };
+
     return (
-        <NavigationContext.Provider value={{}}>
+        <NavigationContext.Provider value={{ currentPath, navigate }}>
+            <div>
+                <button onClick={() => navigate("/accordion")}>
+                    Navigate to Accordion
+                </button>
+            </div>
+            <div>
+                <button onClick={() => navigate("/dropdown")}>
+                    Navigate to Dropdown
+                </button>
+            </div>
             {currentPath}
             {children}
         </NavigationContext.Provider>
