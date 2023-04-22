1fe34f7c6650b40483123a6a854517f4f82d4943
Morne Louw <louw.morne@gmail.com>
2023-04-22 09:20:24 +0200

    listening to browser forwars and back events

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 export const NavigationProvider = ({ children }) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+        window.addEventListener("popstate", handler);
+
+        // clean up function
+        return () => {
+            window.removeEventListener("popstate", handler);
+        };
+    }, []);
+
     return (
         <NavigationContext.Provider value={{}}>
+            {currentPath}
             {children}
         </NavigationContext.Provider>
     );
