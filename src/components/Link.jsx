8e5b246def29ece07d4a6ef0f345d459b287ac71
Morne Louw <louw.morne@gmail.com>
2023-04-23 07:53:48 +0200

    refactor Link to handle open in new tab from keyboard

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,11 +4,22 @@ import NavigationContext from "../context/navigation";
 const Link = ({ to, children }) => {
     const { navigate } = useContext(NavigationContext);
     const handleClick = (event) => {
+        // if the use explicitly forces "Open in New Tab" using Ctrl + click (Windows) or Cmd + click (MacOS),
+        // the we don't want to preventDefault
+        // Windows: event.ctrKey
+        // MacOS: event.metaKey
+        if (event.ctrlKey || event.metaKey) {
+            return;
+        }
         event.preventDefault();
         navigate(to);
     };
 
-    return <a onClick={handleClick}>{children}</a>;
+    return (
+        <a href={to} onClick={handleClick}>
+            {children}
+        </a>
+    );
 };
 
 export default Link;
