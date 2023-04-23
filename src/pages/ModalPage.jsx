4a6b72bc24552e558d453a3174f81759e3a1e949
Morne Louw <louw.morne@gmail.com>
2023-04-23 14:28:06 +0200

    add showModal state and open-modal action

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 
 const ModalPage = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleButtonClick = () => {
+        setShowModal(true);
+    };
+
     return (
         <div>
-            <Modal />
+            <Button primary onClick={handleButtonClick}>
+                Open Modal
+            </Button>
+            {showModal && <Modal />}
         </div>
     );
 };
