e02128a77768da977d374b33f7bab74fa9b44491
Morne Louw <louw.morne@gmail.com>
2023-04-24 19:49:00 +0200

    refactor: make the content of each row dynamic

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -8,7 +8,20 @@ const data = [
     { name: "Lime", color: "bg-green-500", score: 4 },
 ];
 
-const config = [{ label: "Name" }, { label: "Color" }, { label: "Score" }];
+const config = [
+    {
+        label: "Name",
+        render: (fruit) => fruit.name,
+    },
+    {
+        label: "Color",
+        render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+        label: "Score",
+        render: (fruit) => fruit.score,
+    },
+];
 const TablePage = () => {
     return (
         <div>
