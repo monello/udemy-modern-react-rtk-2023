a7e863ea8f029010ab165173f632adf61387dc40
Morne Louw <louw.morne@gmail.com>
2023-04-26 00:48:40 +0200

    add the soring icons

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
 
 const SortableTable = (props) => {
     const { config, data } = props;
@@ -46,8 +47,14 @@ const SortableTable = (props) => {
         return {
             ...column,
             header: () => (
-                <th onClick={() => handleClick(column.label)}>
-                    {column.label} [{column.label === sortBy && sortOrder}]
+                <th
+                    className="cursor-pointer hover:bg-gray-100"
+                    onClick={() => handleClick(column.label)}
+                >
+                    <div className="flex items-center">
+                        {getIcons(column.label, sortBy, sortOrder)}{" "}
+                        {column.label}
+                    </div>
                 </th>
             ),
         };
@@ -62,5 +69,36 @@ const SortableTable = (props) => {
         />
     );
 };
+
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy)
+        return (
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
+
+    if (!sortOrder) {
+        return (
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
+    } else if (sortOrder === "asc") {
+        return (
+            <div>
+                <GoArrowSmallUp />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <GoArrowSmallDown />
+            </div>
+        );
+    }
+}
 
 export default SortableTable;
