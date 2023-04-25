1987fd7ff4661e016969d212433d6e797a8da4a2
Morne Louw <louw.morne@gmail.com>
2023-04-26 00:22:28 +0200

    implement the sorting logic

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Table from "./Table";
 
 const SortableTable = (props) => {
-    const { config } = props;
+    const { config, data } = props;
 
     const [sortOrder, setSortOrder] = useState(null);
     const [sortBy, setSortBy] = useState(null);
@@ -20,6 +20,27 @@ const SortableTable = (props) => {
         }
     };
 
+    let sortedData = data;
+    // only apply sorting if sorting is applicable
+    if (sortBy && sortOrder) {
+        // make a copy of data so we don't edit the original array
+        sortedData = [...data];
+        const reverseOrder = sortOrder === "asc" ? 1 : -1;
+        // desctruture the sortValue function from the config
+        const { sortValue } = config.find((column) => column.label === sortBy);
+        // apply the sorting
+        sortedData.sort((a, b) => {
+            // use the sorValue() function to extract the value from each data-object
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+            if (typeof valueA === "string") {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder;
+            }
+        });
+    }
+
     const updatedConfig = config.map((column) => {
         if (!column.sortValue) return column;
         return {
@@ -32,7 +53,14 @@ const SortableTable = (props) => {
         };
     });
 
-    return <Table {...props} config={updatedConfig} onClick={handleClick} />;
+    return (
+        <Table
+            {...props}
+            config={updatedConfig}
+            data={sortedData}
+            onClick={handleClick}
+        />
+    );
 };
 
 export default SortableTable;
