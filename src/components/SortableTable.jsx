dd2bf0be59f4bcb752d3e7d5fbb115cf6c568d0b
Morne Louw <louw.morne@gmail.com>
2023-04-25 22:29:59 +0200

    adding a sortable-table component

---
+++ src/components/SortableTable.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Table from "./Table";
+
+const SortableTable = (props) => {
+    const { config } = props;
+    const updatedConfig = config.map((column) => {
+        if (!column.sortValue) return column;
+        return {
+            ...column,
+            header: () => <th>{column.label} [^v]</th>,
+        };
+    });
+
+    return <Table {...props} config={updatedConfig} />;
+};
+
+export default SortableTable;
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
@@ -12,6 +13,7 @@ const config = [
     {
         label: "Name",
         render: (fruit) => fruit.name,
+        sortValue: (fruit) => fruit.name,
     },
     {
         label: "Color",
@@ -20,7 +22,7 @@ const config = [
     {
         label: "Score",
         render: (fruit) => fruit.score,
-        header: () => <th className="bg-red-500">Score</th>,
+        sortValue: (fruit) => fruit.score,
     },
 ];
 
@@ -29,7 +31,7 @@ const keyFn = (fruit) => fruit.name;
 const TablePage = () => {
     return (
         <div>
-            <Table data={data} config={config} keyFn={keyFn} />
+            <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     );
 };
