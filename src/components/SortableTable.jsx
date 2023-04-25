450f8e10ef28a1e10496bc753cdfb6ba66d08bb3
Morne Louw <louw.morne@gmail.com>
2023-04-25 22:47:35 +0200

    lisenting for onclick on sortable headers

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -3,15 +3,24 @@ import Table from "./Table";
 
 const SortableTable = (props) => {
     const { config } = props;
+
+    const handleClick = (label) => {
+        console.log(`Label: ${label}`);
+    };
+
     const updatedConfig = config.map((column) => {
         if (!column.sortValue) return column;
         return {
             ...column,
-            header: () => <th>{column.label} [^v]</th>,
+            header: () => (
+                <th onClick={() => handleClick(column.label)}>
+                    {column.label} [^v]
+                </th>
+            ),
         };
     });
 
-    return <Table {...props} config={updatedConfig} />;
+    return <Table {...props} config={updatedConfig} onClick={handleClick} />;
 };
 
 export default SortableTable;
