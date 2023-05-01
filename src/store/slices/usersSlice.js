c185fa5b5cf383ac28efb543fe3116fa01bf4197
Morne Louw <louw.morne@gmail.com>
2023-05-01 21:06:05 +0200

    adding the extraReducers cases for async thunks

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
     name: "users",
@@ -7,7 +8,28 @@ const usersSlice = createSlice({
         isLoading: false,
         error: null
     },
-    reducers: {}
+    // extraReducers is a space to add "listeners" to actions that will be dispatched, but
+    //   we didn't manually add them to the reducers section of this slice
+    // AsyncThunks are a good example of actions that will be dispatched but are dfeined outside this slice
+    //   pending, fullfilled, rejected
+    extraReducers: (builder) => {
+        // builder.addCase('users/fetch/pending'); can be written as builder.addCase(fetchUsers/pending)
+        // RTK adds these properties on for us so we do not have to type the string version manually
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            // NOTE: Whatever is being returned from the async thunk (in this case fetchUsers) will be assigned to the action.payload
+            // The full action argument will be: {type: 'users/fetch/fullfilled, payload: [--- this will containe the data ---]}
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            // in the case of a rejected async thunk the error being returned will be assigned to action.error
+            state.error = action.error;
+        });
+    }
 });
 
 export const usersReducer = usersSlice.reducer;
