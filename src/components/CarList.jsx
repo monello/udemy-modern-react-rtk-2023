4c249e914378fa2eaff3bb367f30366cfc9848cc
Morne Louw <louw.morne@gmail.com>
2023-05-01 12:51:55 +0200

    add functionality to bold exsiting cars while user creates a new car

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -5,30 +5,41 @@ import { carsActions } from "../store";
 const CarList = () => {
     const dispatch = useDispatch();
 
-    const cars = useSelector(({ cars: { searchTerm, data } }) => {
-        const re = new RegExp(searchTerm, "i");
-        return data.filter((car) => car.name.match(re));
-    });
+    const { cars, name } = useSelector(
+        ({ form, cars: { searchTerm, data } }) => {
+            const re = new RegExp(searchTerm, "i");
+            const filteredcars = data.filter((car) => car.name.match(re));
+            return {
+                cars: filteredcars,
+                name: form.name,
+            };
+        }
+    );
 
     const handleCarDelete = (car) => {
         dispatch(carsActions.removeCar(car.id));
     };
 
-    const renderedCars = cars.map((car) => (
-        <div key={car.id} className="panel">
-            <p>
-                {car.name} - ${car.cost}
-            </p>
-            <button
-                className="button is-danger"
-                onClick={() => {
-                    handleCarDelete(car);
-                }}
-            >
-                Delete
-            </button>
-        </div>
-    ));
+    const renderedCars = cars.map((car) => {
+        const isBold =
+            name && car.name.toLowerCase().includes(name.toLowerCase());
+        return (
+            <div key={car.id} className={`panel ${isBold && "bold"}`}>
+                <p>
+                    {car.name} - ${car.cost}
+                </p>
+                <button
+                    className="button is-danger"
+                    onClick={() => {
+                        handleCarDelete(car);
+                    }}
+                >
+                    Delete
+                </button>
+            </div>
+        );
+    });
+
     return (
         <div className="car-list">
             {renderedCars}
