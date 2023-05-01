d64a9377f04afb1501b25ac90f922eb88e10b477
Morne Louw <louw.morne@gmail.com>
2023-05-01 14:21:26 +0200

    initial redux stetup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from "./store";
 import App from './App';
 import './index.css';
 
 const el = document.getElementById("root");
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+    <ReduxProvider store={store}>
+        <App />
+    </ReduxProvider>
+);
