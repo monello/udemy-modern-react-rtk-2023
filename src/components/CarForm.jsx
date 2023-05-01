d77b9d6181f2e30d9f7ffa5019d07383995e215d
Morne Louw <louw.morne@gmail.com>
2023-05-01 06:50:19 +0200

    form field - hookup name field to redux

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { formActions } from "../store";
 
 const CarForm = () => {
-    return <div>CarForm</div>;
+    const dispatch = useDispatch();
+
+    const name = useSelector((state) => state.form.name);
+
+    const handleNameChange = (event) => {
+        dispatch(formActions.changeName(event.target.value));
+    };
+
+    return (
+        <div className="car-form panel">
+            <h4 className="subtitle is-3">Add car</h4>
+            <form>
+                <div className="field-group">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <input
+                            className="input is-expanded"
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default CarForm;
