753e6539736d44b4b4911ed3d36e1580aa2a5afe
Morne Louw <louw.morne@gmail.com>
2023-05-01 11:53:48 +0200

    hookup search field to redux

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { carsActions } from "../store/slices/carsSLice";
 
 const CarSearch = () => {
-    return <div>CarSearch</div>;
+    const dispatch = useDispatch();
+
+    const searchTerm = useSelector((state) => state.cars.searchTerm);
+
+    const handleSearchTermChange = (event) => {
+        dispatch(carsActions.changeSearchTerm(event.target.value));
+    };
+
+    return (
+        <div className="list-header">
+            <h3 className="title is-3">My Cars</h3>
+            <div className="search field is-horizontal">
+                <label className="label">Search</label>
+                <input
+                    type="text"
+                    className="input"
+                    value={searchTerm}
+                    onChange={handleSearchTermChange}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default CarSearch;
