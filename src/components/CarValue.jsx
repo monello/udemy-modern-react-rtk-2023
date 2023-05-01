0797e7686bf882eb5efb25df5f1b904cb1fb51df
Morne Louw <louw.morne@gmail.com>
2023-05-01 12:35:32 +0200

    derived state for total cost on filtred list

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CarValue = () => {
-    return <div>CarValue</div>;
+    const totalCost = useSelector(({ cars: { searchTerm, data } }) => {
+        const re = new RegExp(searchTerm, "i");
+
+        // OPTION 1
+        // const filteredCars = data.filter((car) => car.name.match(re));
+        // return filteredCars.reduce(
+        //     (accumulator, car) => accumulator + car.cost,
+        //     0
+        // );
+
+        // OPTION 2: JS built-in chaining
+        return data
+            .filter((car) => car.name.match(re))
+            .reduce((accumulator, car) => accumulator + car.cost, 0);
+    });
+
+    return <div className="car-value">Total Cost: ${totalCost}</div>;
 };
 
 export default CarValue;
