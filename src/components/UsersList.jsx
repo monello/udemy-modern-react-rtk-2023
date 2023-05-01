70a8b875e3bfe7de023a45e77294a3bab556d872
Morne Louw <louw.morne@gmail.com>
2023-05-01 21:13:13 +0200

    dispatch the fetchUsers async thunk from component

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
 
 const UsersList = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
     return <div>UsersList</div>;
 };
 
