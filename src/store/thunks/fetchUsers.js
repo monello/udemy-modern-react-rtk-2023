632bce59b80185f01cc6e12d39b4d6ce902907eb
Morne Louw <louw.morne@gmail.com>
2023-05-01 21:36:25 +0200

    OOPS restore the pause trick didn't realise I need it for a while

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -8,7 +8,19 @@ import axios from "axios";
 // As mentioned these are generated and dispatched automatically for us by RTK
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get("http://localhost:3005/users");
+
+    // TODO: REMOVE - DEV ONLY!
+    await pause(1000);
+
+
     return response.data;
 });
 
 export { fetchUsers };
+
+// TODO: REMOVE - DEV ONLY!
+const pause = (duration) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, duration);
+    });
+};
