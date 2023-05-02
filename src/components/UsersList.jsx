21d3d15d6dd0549f40213717c4462bd6efd1c34e
Morne Louw <louw.morne@gmail.com>
2023-05-02 23:08:32 +0200

    refactor users-list to always show header

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -22,23 +22,22 @@ const UsersList = () => {
         execAdduser();
     };
 
+    let content;
     if (isLoadingUsers) {
-        return <Skeleton times={6} className="h-10 w-full" />;
-    }
-
-    if (loadingUsersError) {
-        return <div>Error fetching data...</div>;
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between item-center cursor-pointer">
-                    {user.name}
+        content = <Skeleton times={6} className="h-10 w-full" />;
+    } else if (loadingUsersError) {
+        content = <div>Error fetching data...</div>;
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between item-center cursor-pointer">
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        );
-    });
+            );
+        });
+    }
 
     return (
         <div>
@@ -49,7 +48,7 @@ const UsersList = () => {
                 </Button>
                 {creatingUserError && "Error creating user..."}
             </div>
-            {renderedUsers}
+            {content}
         </div>
     );
 };
