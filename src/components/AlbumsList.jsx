c87828b2650397ed49e2247e5b1a78dce30369cf
Morne Louw <louw.morne@gmail.com>
2023-05-06 08:00:13 +0200

    render the albums per user

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
 
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data, error, isLoading);
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />;
+    } else if (error) {
+        content = <div>Error loading albums</div>;
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>;
+            return (
+                <ExpandablePanel key={album.id} header={header}>
+                    List of photos in this album
+                </ExpandablePanel>
+            );
+        });
+    }
 
-    return <div>Albums for {user.name}</div>;
+    return (
+        <div>
+            <div>Albums for {user.name}</div>
+            {content}
+        </div>
+    );
 };
 
 export default AlbumsList;
